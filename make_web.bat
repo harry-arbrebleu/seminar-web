306d5b893a7a5c09f8f0598c987426d163200f6c
Harry Arbrebleu <104429431+harry-arbrebleu@users.noreply.github.com>
2024-02-07 15:53:48 +0900

    add article(automated)

--- make_web.bat
+++ make_web.bat
@@ -3,4 +3,4 @@ git pull origin master
 cd ..\seminar-web
 git pull origin master
 cd ..\seminar-public
-bundle && bundle exec jekyll build && python change.py && git add . && git commit -m "add article(automated)" && git push -u origin master && xcopy /e/Y _site\ ..\seminar-web\ && cd ..\seminar-web && git pull origin master && git add . && git commit -m "add article(automated)" && git push -u origin master && cd ..\seminar-public
+bundle && bundle exec jekyll build && python change.py && git add . && git commit -m "add article(automated)" && git push -u origin master && xcopy /e/Y _site\ ..\seminar-web\ && cd ..\seminar-web && git add . && git commit -m "add article(automated)" && git push -u origin master && cd ..\seminar-public
